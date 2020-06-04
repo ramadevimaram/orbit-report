3e942c26bff313b827744ab6b293c1567e31b152
ramadevimaram <ramamaram.java@gmail.com>
2020-06-03 20:55:13 -0400

    writed the code how to add alternate color in table in orbit-list-component and search function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   /*constructor(){
     this.sourceList = [
       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
@@ -19,6 +20,7 @@ export class AppComponent {
   }*/
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -26,17 +28,30 @@ export class AppComponent {
  
           let fetchedSatellites = data.satellites;
           // TODO: loop over satellites
-          for(let i=0; fetchedSatellites.length > 0; i++){
+          for(let i=0; i < fetchedSatellites.length; i++){
             // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             let satellite= new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);        
             // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
             this.sourceList.push(satellite);
           }
-            
-            
-       }.bind(this));
+              // make a copy of the sourceList to be shown to the user
+          this.displayList = this.sourceList.slice(0);
+        }.bind(this));
     }.bind(this));
  
- }
+ };
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
 
 }
